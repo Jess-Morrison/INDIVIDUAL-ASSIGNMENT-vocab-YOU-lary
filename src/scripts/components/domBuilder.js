3849fb3f265cff6bd70fde0eb4a2ba53261698a3
Jessica Morrison <jnicomorr@gmail.com>
2022-06-06 20:21:05 -0500

    Placed cards on the DOM

--- src/scripts/components/domBuilder.js
+++ src/scripts/components/domBuilder.js
@@ -4,13 +4,13 @@ const domBuilder = () => {
   const domString = `
   <div id='main-container'>
   <div id="navigation"></div>
-  <button id="Javascript" type="button" style="Background-color:RED">Javascript</button>
-  <button id="HTML" type="button"  style="Background-color:BLUE">HTML</button>
-  <button id="CSS" type="button"  style="Background-color:PURPLE">CSS</button>
+  <button id="javascript" type="button" style="Background-color:RED">Javascript</button>
+  <button id="html" type="button"  style="Background-color:BLUE">HTML</button>
+  <button id="css" type="button"  style="Background-color:PURPLE">CSS</button>
   <div id="add-button"></div>
   <div id="form-container"></div>
+  <div id='store'></div>
   <div id= 'view' ></div>
-  <div id="view"></div>
   </div>`;
   renderToDom('#app', domString);
 };
