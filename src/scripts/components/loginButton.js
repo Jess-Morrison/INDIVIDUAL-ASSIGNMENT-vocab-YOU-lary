1456fd016672ab55476058f52a8caac3f5a7d70e
Jessica Morrison <jnicomorr@gmail.com>
2022-08-20 13:21:11 -0500

    try2

--- src/scripts/components/loginButton.js
+++ src/scripts/components/loginButton.js
@@ -4,7 +4,7 @@ import renderToDom from '../helpers/renderToDom';
 // GOOGLE LOGIN BUTTON
 const loginButton = () => {
   const domString = '<button id="google-auth" class="btn btn-danger">GOOGLE LOGIN</button>';
-  // document.querySelector('#login-form-container').innerHTML = domString;
+  document.querySelector('#login-form-container').innerHTML = domString;
   renderToDom('#login-form-container', domString);
   document.querySelector('#google-auth').addEventListener('click', signIn);
 };
