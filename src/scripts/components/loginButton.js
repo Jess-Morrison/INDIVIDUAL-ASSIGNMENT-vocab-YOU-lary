021d24c2d36f5d82cdd020e8b53f54ff7dad9927
Jessica Morrison <jnicomorr@gmail.com>
2022-08-20 10:57:49 -0500

    Fixed login btn

--- src/scripts/components/loginButton.js
+++ src/scripts/components/loginButton.js
@@ -1,9 +1,11 @@
 import signIn from '../helpers/auth/signIn';
+import renderToDom from '../helpers/renderToDom';
 
 // GOOGLE LOGIN BUTTON
 const loginButton = () => {
   const domString = '<button id="google-auth" class="btn btn-danger">GOOGLE LOGIN</button>';
   document.querySelector('#login-form-container').innerHTML = domString;
+  renderToDom('#login-form-container', domString);
   document.querySelector('#google-auth').addEventListener('click', signIn);
 };
 
